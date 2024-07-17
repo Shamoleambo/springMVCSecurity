fdbc833f49577131e019ed6cf625f9855c5c4193
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-17 08:47:32 -0300

    add logout functionality

--- src/main/java/com/tidz/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/tidz/demosecurity/security/DemoSecurityConfig.java
@@ -28,7 +28,8 @@ public class DemoSecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated()).formLogin(
-				form -> form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+				form -> form.loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
+				.logout(logout -> logout.permitAll());
 
 		return http.build();
 	}
